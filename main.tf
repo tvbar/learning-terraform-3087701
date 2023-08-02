aafb71e57bb4b8c4408825c5ed34b0cccf6aaa03
tvbar <139884727+tvbar@users.noreply.github.com>
2023-08-02 16:55:15 +0100

    Post LB

--- main.tf
+++ main.tf
@@ -50,8 +50,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = "module.blog_vpc.vpc_id"
-  subnets            = [module.blog_vpc.public_subnets]
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
   access_logs = {
