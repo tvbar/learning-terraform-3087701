870ba8e80d4e1938cb04bc1a4514abc3717d52ce
tvbar <139884727+tvbar@users.noreply.github.com>
2023-08-02 17:41:39 +0100

    error check

--- main.tf
+++ main.tf
@@ -21,9 +21,11 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = []
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
+  enable_vpn_gateway = false
 
   tags = {
     Terraform = "true"
