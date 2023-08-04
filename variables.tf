7b51872b0e50f0c326bed2df3ab43f498024fa07
tvbar <139884727+tvbar@users.noreply.github.com>
2023-08-04 16:31:48 +0100

    error 2

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "instance_type" {
 }
 
 variable "ami_filter"{
-  description "Name filter and owner for AMI"
+  description = "Name filter and owner for AMI"
 
   type = object({
     name = string
